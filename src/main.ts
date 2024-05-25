7cea10d538b48d585efb51d9e97b744563c29cda
SuryaPratap-ClevDoc <suryapratap@clevdoc.com>
2024-05-26 00:21:32 +0530

    Added tailwind css

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,22 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="flex justify-center items-center bg-blue-400 min-h-screen">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="${viteLogo}" class="w-32" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      </a>
+      <h1>Vite + TypeScript</h1>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and TypeScript logos to learn more
+      </p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
