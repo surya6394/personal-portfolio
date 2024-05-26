fe974e955dfef4b9a12d795689ee33131853c2cc
SuryaPratap-ClevDoc <suryapratap@clevdoc.com>
2024-05-26 23:49:06 +0530

    setuped folder structure

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Elements/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <div className='container mx-auto'>
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default Home
